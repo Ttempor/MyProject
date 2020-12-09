d9924a05e01f4b77c91f74983c1a2487a165fecc
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-12-09 16:46:24 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+	char s1[] = "喊出我的名字吧";
+	char s2[] = "Utlra泽塔！";
+	if(strcmp(s1, s2)) {
+		printf("同\n");
+	} else {
+		printf("不同\n");	
+	}
+}
