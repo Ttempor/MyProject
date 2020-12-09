91902e7293a29c47b28c7d58e367dfc772743aa8
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-12-09 08:51:31 +0800

    Create 120901.数组元素递减.app.c

---
+++ 120901.数组元素递减.app.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main() {
+	int a = 90;
+	int array[9][10];
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < 10; j++) {
+			array[i][j] = 0;
+			printf("%d   ", array[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(int i1 = 0; i1 < 9; i1++) {
+		for(int j1 = 0; j1 < 10; j1++) {
+			array[i1][j1] = a--;
+			printf("%d   ", array[i1][j1]);
+		}
+		printf("\n");
+	}
+}
