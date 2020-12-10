84e6a04f5a561778d89dd5b0e1c99332e3348a58
性能小钢炮 <74130814+Ttempor@users.noreply.github.com>
2020-12-10 20:26:43 +0800

    Update 120905.串长度.app.c

--- 120905.串长度.app.c
+++ 120905.串长度.app.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main() {
 	char s1[] = "喊出我的名字吧";
-	char s2[] = "UtlraMan Zeo!";
+	char s2[] = "UltraMan Zett!";
 	printf("%d\n",sizeof(s1));
 	printf("%d\n",sizeof(s2));	
 }
