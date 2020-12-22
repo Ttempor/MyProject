5842df2f070c1cc332c2f879ec70f553b730ff43
Ttempor <575860351@qq.com>
2020-12-22 23:12:00 +0800

    aaaa

---
+++ src/domain/Employee.java
@@ -0,0 +1,55 @@
+package domain;
+
+//员工类
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private double salary;
+
+    /**
+     * 初始化员工类信息
+     * @param id
+     * @param name
+     * @param age
+     * @param salary
+     */
+    public Employee(int id, String name, int age, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
